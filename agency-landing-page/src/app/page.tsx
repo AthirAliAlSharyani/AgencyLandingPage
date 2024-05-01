9e7f167f19b4378b9861b834bfa39c0aa3d75ef9
aathirr <athiralsharyani@gmail.com>
2024-05-01 10:26:27 +0400

    change content

--- agency-landing-page/src/app/page.tsx
+++ agency-landing-page/src/app/page.tsx
@@ -11,6 +11,8 @@ import {
   Box,
   border,
   Button,
+  Center,
+  Flex,
 } from "@chakra-ui/react";
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from "@chakra-ui/react";
@@ -22,31 +24,40 @@ export default function Home() {
       <ChakraProvider>
         <Box bg="blue.100" height="100vh" width="100vw">
           <NavigationBar />
-          <Box maxW="35rem" ml={"37%"}>
-            <Heading
-              fontFamily={"fantasy"}
-              mb={4}
-              mt={"30%"}
-              ml={"10%"}
-              textAlign={"center"}
-              fontWeight={"100"}
-              fontSize={73.34}
-            >
-              Awesome UI Dark Template for Webflow Agency
-            </Heading>
-            <Button
-              bg={"black"}
-              color={"white"}
-              _hover={{ bg: "white", color: "black" }}
-              ml={"36%"}
-              mt={4}
-              w={"211px"}
-              h={"60px"}
-            >
-              Get in Touch
-            </Button>
-          </Box>
-        </Box>{" "}
+          <Flex
+            direction="column"
+            align="center"
+            justify="center"
+            height="100vh" // This makes the Flex container fill the viewport height
+          >
+            <Box maxW="35rem" alignContent={"center"} mt={"-60"}>
+              <Heading
+                fontFamily={"fantasy"}
+                mb={4}
+                mt={"30%"}
+                textAlign={"center"}
+                fontWeight={"100"}
+                maxW={"450"}
+                w={"200"}
+                fontSize={73}
+              >
+                Awesome UI Dark Template for Webflow Agency
+              </Heading>
+              <Center>
+                <Button
+                  bg={"black"}
+                  color={"white"}
+                  _hover={{ bg: "white", color: "black" }}
+                  mt={4}
+                  w={"211px"}
+                  h={"60px"}
+                >
+                  Get in Touch
+                </Button>
+              </Center>
+            </Box>
+          </Flex>
+        </Box>
       </ChakraProvider>
     </>
   );
