85722b46729f27986db2db3c2d430fe0c67c4a54
aathirr <athiralsharyani@gmail.com>
2024-05-07 09:17:31 +0400

    responsive let get in touch

--- agency-landing-page/src/components/Lets_get_intouch/index.tsx
+++ agency-landing-page/src/components/Lets_get_intouch/index.tsx
@@ -7,6 +7,7 @@ import {
   Heading,
   HStack,
   Input,
+  Stack,
   Textarea,
 } from "@chakra-ui/react";
 import React from "react";
@@ -19,20 +20,20 @@ export default function Lets_Get_InTouch() {
       <Flex
         direction="column"
         align="center"
-        mx="auto"
+        alignContent={"center"}
+        p={90}
         w="full"
-        overflow="hidden"
-        zIndex={1}
-        position="relative" // Relative positioning for the container
+        mt={"auto"}
       >
         {/* Foreground Content */}
         <Box
           mt={10}
           pb={14}
           px={4}
-          w={["400px", "900px", "800"]}
+          w={{ sm: "400px", md: "750px", lg: "500", xl: "900" }}
           maxW="auto"
           zIndex="1"
+          mr={8}
         >
           <Heading
             size="lg"
@@ -44,9 +45,9 @@ export default function Lets_Get_InTouch() {
 
           <FormControl mt={4}>
             <FormLabel>Email</FormLabel>
-            <Input borderColor={"black"} type="email" mb={8} />
+            <Input borderColor={"black"} type="email" mb={8} h={"50px"} />
             <FormLabel>Name</FormLabel>
-            <Input borderColor={"black"} type="text" mb={8} />
+            <Input borderColor={"black"} type="text" mb={8} h={"50px"} />
             <FormLabel>Message</FormLabel>
             <Textarea
               borderColor={"black"}
@@ -57,71 +58,69 @@ export default function Lets_Get_InTouch() {
           <Button
             bg="blue.600"
             color="white"
-            _hover={{ color: "black" }}
+            _hover={{ bg: "white", color: "black", border: "1px solid black" }}
             w="full"
             mt={5}
+            h={"60px"}
           >
             Get in Touch
           </Button>
         </Box>
 
-        <Box
+        <Flex
           bg="white"
-          borderRadius="lg"
-          mb={2}
-          // w={["200px", "900px", "800"]}
-          px={10}
-          py={10}
-          textAlign="center"
-          zIndex="1"
-          w={["400px", "900px", "800"]}
-          maxW="auto"
+          borderRadius="26"
+          mt={10}
+          pb={14}
+          px={4}
+          w={{ sm: "400px", md: "950px", lg: "500", xl: "900" }}
         >
-          <Heading mb={4} fontSize={["md", "lg", "xl"]} mr={"70%"}>
-            Agency landing page
-          </Heading>
-          <HStack>
+          <Stack>
+            <HStack
+              fontSize={["5", "10", "12px"]}
+              mb={7}
+              w={{ sm: "100px", md: "900px", lg: "1200" }}
+            >
+              <Heading mb={4} mt={8} fontSize={["sm", "md", "xl"]} mr={"50%"}>
+                Agency page
+              </Heading>
+              <HStack color={"gray.400"} w={["100px", "280px"]}>
+                <Text> Home</Text>
+                <Text> About</Text>
+                <Text w={["70px", "80px"]}>How it Works</Text>
+                <Text> Services</Text>
+              </HStack>
+            </HStack>
             <Text
-              fontSize={["sm", "md", "lg"]}
+              fontSize={["10", "10", "lg"]}
               mx={[2, 1, 8]}
               textAlign="left"
               color={"gray.400"}
-              mr={"auto"}
-              noOfLines={3}
-              w={350}
+              m={"auto"}
+              w={{ sm: "200px", md: "500px", lg: "200" }}
             >
               Ready to elevate your online presence? Contact us today to discuss
               your project and discover how we can bring your vision to life.
             </Text>
-            <HStack
-              fontSize={["xs", "md", "md"]}
-              color={"gray.400"}
-              gap={6}
-              mb={20}
-              ml={"15%"}
-            >
-              <Text> Home</Text>
-              <Text> About</Text>
-              <Text noOfLines={1} w={"20"}>
-                How it Works
-              </Text>
-              <Text> Services</Text>
+            <HStack mt={7} gap={{ sm: "20", md: "60", lg: "480" }}>
+              <HStack>
+                <ChatIcon />
+                <Text
+                  color={"gray.400"}
+                  fontSize={["11", "10", "15"]}
+                  w={["30px", "60px", "120px"]}
+                >
+                  {" "}
+                  Made with love
+                </Text>
+              </HStack>
+              <HStack>
+                <SunIcon mr={6} /> <BellIcon mr={6} /> <ChatIcon mr={6} />{" "}
+                <EditIcon mr={10} />
+              </HStack>
             </HStack>
-          </HStack>
-          <HStack justifyContent={"space-between"} mt={5}>
-            <HStack ml={"7"}>
-              <ChatIcon />
-              <Text color={"gray.400"} fontSize={["xs", "sm", "md"]}>
-                {" "}
-                Made with love
-              </Text>
-            </HStack>
-            <Box mx={"3%"} ml={"15%"} gap={6}>
-              <SunIcon mr={10} /> <BellIcon mr={10} /> <ChatIcon mr={10} />{" "}
-              <EditIcon mr={10} />
-            </Box>
-          </HStack>
-        </Box>
+          </Stack>
+        </Flex>
       </Flex>
     </ChakraProvider>
   );
