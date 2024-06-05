c3ad00a49bb869f252445c3f0d4fd7a40fdeb17c
aathirr <athiralsharyani@gmail.com>
2024-06-05 11:39:28 +0400

    changes

--- agency-landing-page/components/what_we_do/index.tsx
+++ agency-landing-page/components/what_we_do/index.tsx
@@ -19,6 +19,7 @@ import {
   Stack,
   VStack,
   Link,
+  ChakraProvider,
 } from "@chakra-ui/react";
 import link from "next/link";
 import React, { useEffect, useState } from "react";
@@ -59,117 +60,123 @@ export default function What_We_Do() {
 
   return (
     <>
-      <GlobalStyle />
-      <Flex
-        direction="column"
-        align="left"
-        alignContent={"center"}
-        p={90}
-        w="full"
-        mt={"auto"}
-      >
-        <HStack
-          justifyContent={"space-between"}
-          alignItems={"center"}
-          flexWrap={{ base: "wrap", md: "nowrap" }}
-          p={4}
+      <ChakraProvider>
+        <Flex
+          direction="column"
+          align="left"
+          alignContent={"center"}
+          p={90}
+          w="full"
+          mt={"auto"}
         >
-          <Heading
-            as="h2"
-            size={{ base: "md", md: "lg", lg: "xl" }}
-            noOfLines={1}
-            ml={{ base: 2, md: 4, lg: 7 }}
-            mb={{ base: 2, md: 0 }}
+          <HStack
+            justifyContent={"space-between"}
+            alignItems={"center"}
+            flexWrap={{ base: "wrap", md: "nowrap" }}
+            p={4}
           >
-            Available Product
-          </Heading>
-
-          <Menu>
-            <MenuButton
-              as={Button}
-              bg={"blue.500"}
-              color={"white"}
-              _hover={{ bg: "white", color: "black" }}
-              mt={{ base: 2, md: 4 }}
-              w={{ base: "100%", md: "211px" }}
-              h={{ base: "40px", md: "60px" }}
-              mr={{ base: 0, md: 20 }}
+            <Heading
+              as="h2"
+              size={{ base: "md", md: "lg", lg: "xl" }}
+              noOfLines={1}
+              ml={{ base: 2, md: 4, lg: 7 }}
+              mb={{ base: 2, md: 0 }}
             >
-              {selectedCategory ? selectedCategory : "Select Category"}
-              <ChevronDownIcon ml={2} />
-            </MenuButton>
-            <MenuList color={"black"}>
-              {categories.map((category) => (
+              Available Product
+            </Heading>
+
+            <Menu>
+              <MenuButton
+                as={Button}
+                bg={"blue.500"}
+                color={"white"}
+                _hover={{ bg: "white", color: "black" }}
+                mt={{ base: 2, md: 4 }}
+                w={{ base: "100%", md: "211px" }}
+                h={{ base: "40px", md: "60px" }}
+                mr={{ base: 0, md: 20 }}
+              >
+                {selectedCategory ? selectedCategory : "Select Category"}
+                <ChevronDownIcon ml={2} />
+              </MenuButton>
+              <MenuList color={"black"}>
+                {categories.map((category) => (
+                  <MenuItem
+                    color={"black"}
+                    key={category}
+                    onClick={() => handleCategorySelect(category)}
+                    _hover={{ bgColor: "blue.200" }}
+                  >
+                    {category}
+                  </MenuItem>
+                ))}
+
                 <MenuItem
-                  color={"black"}
-                  key={category}
-                  onClick={() => handleCategorySelect(category)}
+                  as="a"
+                  href="./Products"
                   _hover={{ bgColor: "blue.200" }}
                 >
-                  {category}
+                  Show All
                 </MenuItem>
-              ))}
-
-              <MenuItem
-                as="a"
-                href="./Products"
-                _hover={{ bgColor: "blue.200" }}
+              </MenuList>
+            </Menu>
+          </HStack>
+          <Grid
+            templateRows={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)" }}
+            templateColumns={{ base: "repeat(2, 1fr)", md: "repeat(4, 1fr)" }}
+            gap={{ base: 2, lg: 4 }}
+            p={2}
+            mt="10"
+            w="full"
+          >
+            {/* Example of repeating GridItem */}
+            {filteredProducts.map((product) => (
+              <GridItem
+                key={product.id}
+                colSpan={1}
+                bg="white"
+                borderRadius={15}
               >
-                Show All
-              </MenuItem>
-            </MenuList>
-          </Menu>
-        </HStack>
-        <Grid
-          templateRows={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)" }}
-          templateColumns={{ base: "repeat(2, 1fr)", md: "repeat(4, 1fr)" }}
-          gap={{ base: 2, lg: 4 }}
-          p={2}
-          mt="10"
-          w="full"
-        >
-          {/* Example of repeating GridItem */}
-          {filteredProducts.map((product) => (
-            <GridItem key={product.id} colSpan={1} bg="white" borderRadius={15}>
-              <VStack>
-                <Heading
-                  as="h2"
-                  fontSize={{ base: "10px", md: "15px", lg: "25px" }}
-                  ml={3}
-                  mt={3}
+                <VStack>
+                  <Heading
+                    as="h2"
+                    fontSize={{ base: "10px", md: "15px", lg: "25px" }}
+                    ml={3}
+                    mt={3}
+                  >
+                    {product.title}
+                  </Heading>
+                  <Image
+                    src={product.image}
+                    alt={product.title}
+                    w={{ base: "100px", md: "150px" }}
+                    h={{ base: "150px", md: "200px" }}
+                  />
+                </VStack>
+                <Box
+                  className="description"
+                  style={{
+                    display: "-webkit-box",
+                    WebkitLineClamp: 3,
+                    WebkitBoxOrient: "vertical",
+                    overflow: "hidden",
+                  }}
                 >
-                  {product.title}
-                </Heading>
-                <Image
-                  src={product.image}
-                  alt={product.title}
-                  w={{ base: "100px", md: "150px" }}
-                  h={{ base: "150px", md: "200px" }}
-                />
-              </VStack>
-              <Box
-                className="description"
-                style={{
-                  display: "-webkit-box",
-                  WebkitLineClamp: 3,
-                  WebkitBoxOrient: "vertical",
-                  overflow: "hidden",
-                }}
-              >
-                <Text
-                  maxW={{ base: "150px", md: "300px" }}
-                  ml={3}
-                  mt={3}
-                  mb={4}
-                  fontSize={{ base: "10px", md: "15px", lg: "20px" }}
-                >
-                  {product.description}
-                </Text>
-              </Box>
-            </GridItem>
-          ))}
-        </Grid>
-      </Flex>
+                  <Text
+                    maxW={{ base: "150px", md: "300px" }}
+                    ml={3}
+                    mt={3}
+                    mb={4}
+                    fontSize={{ base: "10px", md: "15px", lg: "20px" }}
+                  >
+                    {product.description}
+                  </Text>
+                </Box>
+              </GridItem>
+            ))}
+          </Grid>
+        </Flex>
+      </ChakraProvider>
     </>
   );
 }
