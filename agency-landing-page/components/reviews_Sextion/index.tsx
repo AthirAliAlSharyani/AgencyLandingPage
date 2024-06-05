0c961fe02ce5768d73700b83885918ae69c32ef5
aathirr <athiralsharyani@gmail.com>
2024-06-05 12:16:26 +0400

    changes

--- agency-landing-page/components/reviews_Sextion/index.tsx
+++ agency-landing-page/components/reviews_Sextion/index.tsx
@@ -20,8 +20,6 @@ export default function Reviews() {
     lg: false,
     xl: false,
   });
-  const bgColor = useColorModeValue("white", "gray.800");
-  const textColor = useColorModeValue("black", "white");
 
   return (
     <ChakraProvider>
