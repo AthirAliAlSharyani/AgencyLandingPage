77d1f7334e26040eb1d99d90e3be5d02453aa6c3
aathirr <athiralsharyani@gmail.com>
2024-06-11 12:26:59 +0400

    changes

--- agency-landing-page/auth/index.ts
+++
@@ -1,8 +0,0 @@
-import NextAuth from "next-auth/next";
-export const BASE_PATH ='/api/auth'
-
-const autOptions: NextAuthConfig ={
-    provider: [],
-    basePath: BASE_PATH,
-    secret: process.env.NEXTAUTH_SECRET,
-}
