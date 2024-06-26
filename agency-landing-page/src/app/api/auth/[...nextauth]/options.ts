2cd630f96e93fb17b9475b811c278e468c499d10
aathirr <athiralsharyani@gmail.com>
2024-06-26 12:00:04 +0400

    Added Google Auth provider

--- agency-landing-page/src/app/api/auth/[...nextauth]/options.ts
+++ agency-landing-page/src/app/api/auth/[...nextauth]/options.ts
@@ -1,13 +1,19 @@
 import type { NextAuthOptions } from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
 import CredentialsProvider from 'next-auth/providers/credentials'
-
+import GoogleProvider from 'next-auth/providers/google'
 export const options: NextAuthOptions = {
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
         }),
+
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
+
         CredentialsProvider({
             name: "Credentials",
             credentials: {
@@ -36,4 +42,5 @@ export const options: NextAuthOptions = {
             }
         })
     ],
+
 }
